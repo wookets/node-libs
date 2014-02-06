24d73ca0d196bf1d7e6a27601084003ccd841daa
Sean Wesenberg <wookets@gmail.com>
2014-02-06 13:59:07 -0600

    0.3.0 removed recursion, because require doesn't work like that normally and it may lead to unnecessary bugs
    Signed-off-by: Sean Wesenberg <wookets@gmail.com>

--- index.js
+++ index.js
@@ -2,33 +2,20 @@
 var fs = require('fs');
 var path = require('path');
 
-var tree = {};
+var libs = {};
 
-function camelCase(string) {
-  return string.replace( /-([a-z])/ig, function(all, letter) {
-    return letter.toUpperCase();
-  });
-}
+var directory = __dirname + '/../../lib';
+var files = fs.readdirSync(directory);
+files.forEach(function(file) {
+  // strip file extension
+  var name = path.basename(file, path.extname(file));
+  // replace - with snakeCase
+  name = name.replace(/-([a-z])/ig, function(all, letter) {return letter.toUpperCase();});
+  // replace . with _
+  //name = name.replace(/\./ig, '_');
+  // use full path for require
+  file = path.join(directory, file);
+  libs[name] = require(file);
+});
 
-function walk(directory, branch) {
-  var files = fs.readdirSync(directory);
-  files.forEach(function(file) {
-    var name = path.basename(file, path.extname(file));
-    name = camelCase(name);
-    file = path.join(directory, file)
-    var stat = fs.statSync(file);
-    if (stat && stat.isDirectory()) {
-      branch[name] = {};
-      walk(file, branch[name]);
-    } else {
-      if (path.extname(file) === '.coffee' || path.extname(file) === '.js') {
-        branch[name] = require(file);
-      }
-    }
-  });
-}
-
-walk(__dirname + '/../../lib', tree);
-
-module.exports = tree;
-
+module.exports = libs;
--- test/test.js
+++ test/test.js
@@ -5,10 +5,12 @@ describe('libs', function() {
 
   it('should read in libs', function() {
     var libs = require('./node_modules/libs');
+    console.log('libs', libs);
     assert.equal(libs.meow, 'meow');
     assert.equal(libs.pants, 'pants');
     assert.equal(libs.pantsMan, 'pants-man');
-    assert.equal(libs.subDir.subber, 'subber');
+    //assert.equal(libs.some_file, 'dotfile');
+    assert.equal(libs.subDir, 'subdir');
   });
 
 })
